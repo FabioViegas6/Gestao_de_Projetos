c67703877f477b0cf7f703fa2631c6b1c9e83fc1
1012469 <1012469@sal.ipg.pt>
2021-11-03 04:21:24 +0100

    Add project files.

---
+++ Gestao_de_Projetos/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gestao_de_Projetos.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Gestao_de_Projetos/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Gestao_de_Projetos.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

2cb0b9d5f5dc7454c450b60f5a0e01b7bf3fa7f4
selmirafernandes <48446323+selmirafernandes@users.noreply.github.com>
2021-11-28 17:53:19 +0000

    Criar base dados Gestao_de_ProjetosContext

---
+++ Gestao_de_Projetos/Data/Gestao_de_ProjetosContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gestao_de_Projetos.Data
+{
+    public class Gestao_de_ProjetosContext : DbContext
+    {
+        public Gestao_de_ProjetosContext(DbContextOptions<Gestao_de_ProjetosContext> options)
+           : base(options)
+        {
+        }
+    }
+}

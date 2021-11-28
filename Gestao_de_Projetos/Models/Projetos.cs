741f4cb9c4abc8975fcc30de8d55951577711579
selmirafernandes <48446323+selmirafernandes@users.noreply.github.com>
2021-11-28 18:09:47 +0000

    Criada classe para representar os projetos

---
+++ Gestao_de_Projetos/Models/Projetos.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gestao_de_Projetos.Models
+{
+    public class Projetos
+    {
+        [Key]
+        public int ID_projeto { get; set; }
+
+        [Display(Name ="Nome do projeto é obrigatorio")]
+        public string Nome_projeto { get; set; }
+
+        [Display(Name = "Campo obrigatorio")]
+        public DateTime Data_inicio { get; set; }
+
+        [Display(Name = "Campo obrigatorio")]
+        public DateTime Data_fim { get; set; }
+    }
+}

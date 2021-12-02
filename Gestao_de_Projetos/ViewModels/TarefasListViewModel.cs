90a58ef37b57ab851439f6878a23700aa2bc2f36
FabioViegas6 <fabioviegas16.fv@gmail.com>
2021-12-02 18:45:04 +0100

    criado a classe tarefas VIemModel

---
+++ Gestao_de_Projetos/ViewModels/TarefasListViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gestao_de_Projetos.ViewModels
+{
+    public class TarefasListViewModel
+    {
+    }
+}

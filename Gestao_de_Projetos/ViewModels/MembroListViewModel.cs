5d77ddb0fd000fdf81dc89a4bd4e04b5eec9876c
selmirafernandes <48446323+selmirafernandes@users.noreply.github.com>
2021-12-02 15:50:40 +0000

    Criada classe MembrosListViewModel

---
+++ Gestao_de_Projetos/ViewModels/MembroListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Gestao_de_Projetos.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gestao_de_Projetos.ViewModels
+{
+    public class MembroListViewModel
+    {
+        public IEnumerable<Membros> ListaMembros { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+    }
+}

66afdc1d7700cc2a0bc77c458b456eb4a2e8a561
FabioViegas6 <fabioviegas16.fv@gmail.com>
2021-12-09 00:09:28 +0100

    criado a classe cliente

---
+++ Gestao_de_Projetos/Models/Clientes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gestao_de_Projetos.Models
+{
+    public class Clientes
+    {
+        public String Nome { get; set; }
+
+        public String Apelido { get; set; }
+
+        public String Contacto { get; set; }
+
+        public String NIF { get; set; }
+
+        public String Email { get; set; }
+    }
+}

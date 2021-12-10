61513165907c3f14e3167b55421b290a1302c648
FabioViegas6 <fabioviegas16.fv@gmail.com>
2021-12-10 13:19:55 +0100

    criado a class estado projeto

---
+++ Gestao_de_Projetos/Models/Estado_Projeto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gestao_de_Projetos.Models
+{
+    public class Estado_Projeto
+    {
+
+        [Key]
+        public int IdEstado { get; set; }
+
+        [Required]
+        [Display(Name = "Estado Projeto")]
+        public string Estado_Projeto { get; set; }
+    }
+}

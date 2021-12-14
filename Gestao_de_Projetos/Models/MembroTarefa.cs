3736d32d6f8616f8f238c1b18d63e20ac5cb45f8
selmirafernandes <48446323+selmirafernandes@users.noreply.github.com>
2021-12-14 20:15:26 +0000

    Criada model Membro tarefas

--- Gestao_de_Projetos/Models/MembroTarefa.cs
+++ Gestao_de_Projetos/Models/MembroTarefa.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,32 @@ namespace Gestao_de_Projetos.Models
 {
     public class MembroTarefa
     {
+        [ForeignKey("FK_IdTarefas")]
+        public int IdTarefas { get; set; }
+        public Tarefas Tarefas { get; set; }
+
+        [ForeignKey("FK_ID_membro")]
+        public int ID_membro { get; set; }
+        public Membros Membros { get; set; }
+
+        [Required]
+        [Display(Name = "Data Prevista Inicio ")]
+        [DataType(DataType.Date)]
+        public DateTime DataPrevistaInicio { get; set; }
+
+        [Required]
+        [Display(Name = "Data Efetiva Inicio ")]
+        [DataType(DataType.Date)]
+        public DateTime DataEfetivaInicio { get; set; }
+
+        [Required]
+        [Display(Name = "Data  prevista Fim ")]
+        [DataType(DataType.Date)]
+        public DateTime DataPrevistaFim { get; set; }
+
+        [Required]
+        [Display(Name = "Data Efetiva Fim ")]
+        [DataType(DataType.Date)]
+        public DateTime DataEfetivaFim { get; set; }
     }
 }

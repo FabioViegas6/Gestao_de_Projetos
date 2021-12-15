6cf2ce2127c7b37eda2105ad1514ab5a043be1c1
selmirafernandes <48446323+selmirafernandes@users.noreply.github.com>
2021-12-15 10:14:53 +0000

    Addicao chaves models projetos

--- Gestao_de_Projetos/Models/Projetos.cs
+++ Gestao_de_Projetos/Models/Projetos.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,19 +12,18 @@ namespace Gestao_de_Projetos.Models
         [Key]
         public int ID_projeto { get; set; }
 
+        [ForeignKey("FK_ClientesId")]
+        public int ClientesId { get; set; }
+        public Clientes Clientes { get; set; }
+
         [Required]
         [Display(Name ="Nome do Projeto")]
         public string Nome_projeto { get; set; }
 
-        [Required]
-        [Display(Name = "Data Inicio")]
-        [DataType(DataType.Date)]
-        public DateTime Data_inicio { get; set; }
+        [ForeignKey("FK_ID_Estado")]
+        public int ID_Estado { get; set; }
+        public Estado_Projeto Estado_Projetos { get; set; }
 
-        [Required]
-        [Display(Name = "Data Fim")]
-        [DataType(DataType.Date)]
-        public DateTime Data_fim { get; set; }
 
 
         /// <summary>

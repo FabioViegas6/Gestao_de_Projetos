58c9deed34a0ee4455d898be731dffd3805f823d
selmirafernandes <48446323+selmirafernandes@users.noreply.github.com>
2022-01-16 00:02:23 +0000

    Troca lingua nosbotoes

--- Gestao_de_Projetos/Startup.cs
+++ Gestao_de_Projetos/Startup.cs
@@ -30,8 +30,8 @@ namespace Gestao_de_Projetos
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
                     Configuration.GetConnectionString("DefaultConnection")));
-            //services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-            //    .AddEntityFrameworkStores<ApplicationDbContext>();
+           // services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            //  .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
             services.AddRazorPages();
 

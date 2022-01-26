8da18819b7d813351893ac6abcbcc04270d35179
selmirafernandes <48446323+selmirafernandes@users.noreply.github.com>
2022-01-26 23:49:48 +0000

    ultima alteracao da pageInfo

--- Gestao_de_Projetos/ViewModels/PagingInfo.cs
+++ Gestao_de_Projetos/ViewModels/PagingInfo.cs
@@ -10,7 +10,7 @@ namespace Gestao_de_Projetos.ViewModels
 		public const int NUMBER_PAGES_SHOW_BEFORE_AFTER = 5;
 
 		public int TotalItems { get; set; }
-		public int PageSize { get; set; } = 2;
+		public int PageSize { get; set; } = 4;
 		public int CurrentPage { get; set; }
 		public int TotalPages =>
 			(int)Math.Ceiling((double)TotalItems / PageSize);
